71d8e53d0147d9fdadf61239e408ce9d9e8600c6
Maskim <maxleon1611@gmail.com>
2025-04-22 02:26:03 +0300

    User model extending AbstractBaseUser

--- server/apps/accounts/models.py
+++ server/apps/accounts/models.py
@@ -1,3 +1,66 @@
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
-# Create your models here.
+from apps.common.models import IsDeletedModel
+
+
+class User(PermissionsMixin,AbstractBaseUser,IsDeletedModel):
+    """
+    Custom user model extending AbstractBaseUser
+    Attributes:
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+        email (EmailField): The email address of the user, used as the username field.
+        avatar (ImageField): The avatar image of the user.
+        is_staff (bool): Designates whether the user can log into this admin site.
+        is_active (bool): Designates whether this user should be treated as active.
+        account_type (str): The type of account (SELLER or BUYER).
+
+    Methods:
+        full_name(): Returns the full name of the user.
+        __str__(): Returns the string representation of the user.
+
+    """
+    ACCOUNT_TYPE=(
+        ('SELLER','SELLER'),
+        ('BUYER','BUYER'),
+    )
+    first_name = models.CharField(max_length=30,verbose_name='Имя',null=True)
+    last_name = models.CharField(max_length=30,verbose_name='Фамилия',null=True)
+    email = models.EmailField(unique=True,verbose_name='Электронная почта')
+    avatar=models.ImageField(
+        verbose_name='Аватар',
+        upload_to='avatars/%Y/%m/%d',
+        blank=True,
+        default='avatars/default.png',
+        validators=[FileExtensionValidator(['png', 'jpg', 'jpeg'])]
+    )
+    is_staff = models.BooleanField(default=False,verbose_name='Сотрудник')
+    is_active = models.BooleanField(default=True,verbose_name='Активен')
+    account_type=models.CharField(
+        choices=ACCOUNT_TYPE,
+        max_length=6,
+        default='BUYER',
+        verbose_name='Тип пользователя'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name','last_name']
+
+    objects = CustomUserManager()
+
+    @property
+    def full_name(self):
+        """return full name"""
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        """string representation of user(full name + email)"""
+        return f'{self.full_name}-{self.email}'
+
+
+
+
+
+
--- server/apps/common/managers/delete.py
+++ server/apps/common/managers/delete.py
@@ -14,13 +14,17 @@ class IsDeletedQuerySet(GetOrNoneQuerySet):
 class IsDeletedManager(GetOrNoneManager):
     """custom manager for hard and soft delete"""
     def get_queryset(self):
+        """manager returns not deleted objects by default"""
         return IsDeletedQuerySet(self.model, using=self._db).filter(is_deleted=False)
 
     def unfiltered(self):
+        """give access for all objects(include deleted)"""
         return IsDeletedQuerySet(self.model,using=self._db)
 
     def hard_delete(self):
+        """delete all objects-> MyModel.objects.hard_delete()"""
         return self.unfiltered().delete(hard_delete=True)
 
     def deleted(self):
+        """view deleted objects"""
         return self.unfiltered().filter(is_deleted=True)
