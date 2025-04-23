a2c32c3ecd61fd8263a0ab21bfcfd4db12dae459
Maskim <maxleon1611@gmail.com>
2025-04-23 18:52:25 +0300

    shipping address model with PhoneNumberField

--- server/apps/profiles/models.py
+++ server/apps/profiles/models.py
@@ -1,3 +1,40 @@
-from django.db import models
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+from apps.common.models import BaseModel
+from phonenumber_field.modelfields import PhoneNumberField
+
+class ShippingAddress(BaseModel):
+    """
+    Represents a shipping address associated with a user.
+
+    Attributes:
+        user (ForeignKey): The user who owns the shipping address.
+        full_name (str): The full name of the recipient.
+        email (EmailField): The email address of the recipient.
+        phone (PhoneNumberField): The phone number of the recipient.
+        address (str): The street address of the recipient.
+        city (str): The city of the recipient.
+        country (str): The country of the recipient.
+        zipcode (str): The postal code of the recipient.
+    Methods:
+        __str__():
+            Returns a string representation of the shipping details.
+    """
+    user=models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='shipping_address'
+    )
+    full_name=models.CharField(max_length=255)
+    email=models.EmailField()
+    phone=PhoneNumberField(null=True,region='BY')
+    address=models.CharField(max_length=255,null=True)
+    country=models.CharField(max_length=200,null=True)
+    zipcode=models.CharField(max_length=6,null=True)
+
+    def __str__(self):
+        return f"{self.full_name}'s shipping details"
+
+
+
